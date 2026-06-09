7fd32663213757971960095a1e92bcc968ab2078
AdityaGit-1 <tiwariaditya199153@gmail.com>
2026-06-09 22:55:47 +0000

    Variables in javascript

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "random@gmail.com"
+var accountPassword = "456"
+accountCity = "Bhonsor"
+
+// accountId = 2 {Not Allowed}
+
+accountEmail = "change@gmail.com"
+accountPassword = "765"
+accountCity = "Pune"
+
+console.log(accountId);
+console.table([accountId , accountEmail , accountPassword , accountCity]);
+
+// Avoid using 'var' for variables to avoid block scope and functional scope issue
+
