159aee25ea17761ce4e0afbade594fb76962ffe8
AdityaGit-1 <tiwariaditya199153@gmail.com>
2026-06-09 23:45:07 +0000

    conversion of DataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = null
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//conversion
+//"33" => 33
+//"33abc" => NaN
+//true or false => 1 or 0
+//null => 0
+
+let isLoggedin =1
+let booleanIsLogged = Boolean(isLoggedin)
+console.log(booleanIsLogged);
+
+// 1 => true ; 0 => false
+// "" => false
+// "Aditya" => true
+
+let anyNumber = 45
+let stringNumber = String(anyNumber)
+console.log(stringNumber);
+
+
+
+
+
